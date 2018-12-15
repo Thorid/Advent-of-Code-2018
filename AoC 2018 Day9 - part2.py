c7ff8b909262bb3b7a110e25d763bfc2c1a33310
Thorid <45050424+Thorid@users.noreply.github.com>
2018-12-15 15:04:22 +0100

    Add files via upload

---
+++ AoC 2018 Day9 - part2.py
@@ -0,0 +1,34 @@
+from blist import blist
+
+def marblesGameTurn(currentMarble, currentPosition, marbleArray, currentPlayer, playersScoreDict):
+    if currentMarble % 23 == 0:
+        for i in range(7):
+            currentPosition -= 1
+            if currentPosition < 0:
+                currentPosition = len(marbleArray) - 1
+        playersScoreDict[currentPlayer] += currentMarble + marbleArray[currentPosition]
+        del marbleArray[currentPosition]
+    else:
+        for i in range(2):
+            currentPosition += 1
+            if currentPosition > len(marbleArray):
+                currentPosition = 1
+        marbleArray.insert(currentPosition, currentMarble)
+    currentMarble += 1
+    currentPlayer += 1
+    return currentMarble, currentPosition, marbleArray, currentPlayer, playersScoreDict
+
+currentMarble = 1
+currentPosition = 0
+marbleArray = blist([0])
+currentPlayer = 0
+numberOfTurns = 7143600
+numberOfPlayers = 466
+playersScoreDict = {}
+for i in range(1,numberOfPlayers+1):
+    playersScoreDict[i] = 0
+for j in range(numberOfTurns):
+    currentMarble, currentPosition, marbleArray, currentPlayer, playersScoreDict = marblesGameTurn(currentMarble, currentPosition, marbleArray, currentPlayer, playersScoreDict)
+    if currentPlayer > numberOfPlayers:
+        currentPlayer = 1
+print(max(playersScoreDict.values()))
